d5a7d7a4810ebc3b612f2a9a33ba300c8957f996
Milton Cardoso <miltonamc@gmail.com>
2019-07-12 23:59:14 +0000

    implemented almost all FileManager methods and the upload method from FileTransfer

--- src/org/academiadecodigo/codezillas/Utils/FileManager.java
+++ src/org/academiadecodigo/codezillas/Utils/FileManager.java
@@ -1,13 +1,54 @@
 package org.academiadecodigo.codezillas.Utils;
 
-import java.io.File;
+import java.io.*;
 
 public abstract class FileManager {
 
-    public static void saveFile(File file){}
-    public static File loadFile(String filepath){return null;}
-    public boolean pathExists(){return false;}
-    public static void choosePath(String path){}
-    public static void createDirectory(String path){}
+    public static void saveFile(File file){
+
+        try {
+
+            InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+            FileOutputStream fileOutputStream = new FileOutputStream(Defaults.ROOT + "test.txt");
+
+            byte[] buffer = new byte[1024];
+            int numOfBytes = inputStream.read(buffer);
+
+            while (numOfBytes != -1){
+                fileOutputStream.write(buffer,0, numOfBytes);
+                numOfBytes = inputStream.read(buffer);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static File loadFile(String filepath){
+
+        File file = new File(filepath);
+
+        if (file.exists()){
+            return file;
+        }
+        return null;
+    }
+
+    public boolean pathExists(String receivedPath){
+
+        File path = new File(receivedPath);
+        return path.isDirectory();
+    }
+
+    public static void choosePath(String path){
+
+    }
+
+    public static void createDirectory(String path){
+
+        File directory = new File(path);
+        if (!directory.exists()){
+            boolean result = directory.mkdir();
+        }
+    }
 
 }
--- src/org/academiadecodigo/codezillas/Utils/FileTransferer.java
+++ src/org/academiadecodigo/codezillas/Utils/FileTransferer.java
@@ -1,13 +1,28 @@
 package org.academiadecodigo.codezillas.Utils;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
 
 public abstract class FileTransferer {
     //can be made non-abstract if necessary
 
-    public static void upload(OutputStream outputStream, File file){};
-    public static File download(InputStream inputStream, String path){return null;}
+    public static void upload(OutputStream outputStream, File file){
+
+        if (file.exists()){
+
+            try {
+                outputStream.write(Files.readAllBytes(file.toPath()));
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    public static File download(InputStream inputStream, String path){
+        return null;
+    }
 
 }
