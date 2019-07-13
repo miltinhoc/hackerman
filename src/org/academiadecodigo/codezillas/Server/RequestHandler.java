1c21cd4593fe6d19774fff1723af165852f1c5c8
Alex M <alexandre.mendonza@gmail.com>
2019-07-13 20:57:31 +0000

    deleted useless class

--- src/org/academiadecodigo/codezillas/Server/RequestHandler.java
+++ src/org/academiadecodigo/codezillas/Server/RequestHandler.java
@@ -1,4 +1,27 @@
 package org.academiadecodigo.codezillas.Server;
 
+import org.academiadecodigo.bootcamp.InputScanner;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
+import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+
 public class RequestHandler {
+
+    public InputScanner stringScanner(String message){
+        StringInputScanner stringScanner = new StringInputScanner();
+        stringScanner.setMessage(message);
+
+        return stringScanner;
+    }
+
+    public void intScanner(String message){
+
+    }
+
+    private InputScanner showMenu(String[] options, String message){
+        MenuInputScanner menuInput = new MenuInputScanner(options);
+        menuInput.setMessage(message);
+
+        return menuInput;
+    }
+
 }
