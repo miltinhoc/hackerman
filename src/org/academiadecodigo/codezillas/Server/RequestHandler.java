6d565b5e7a7665e494c48f0a9cca7cc2afb6726d
Milton Cardoso <miltonamc@gmail.com>
2019-07-13 22:25:05 +0000

    implemented handleStart methon in RequestHandler class

--- src/org/academiadecodigo/codezillas/Server/RequestHandler.java
+++ src/org/academiadecodigo/codezillas/Server/RequestHandler.java
@@ -3,8 +3,11 @@ package org.academiadecodigo.codezillas.Server;
 import org.academiadecodigo.bootcamp.InputScanner;
 import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
 import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+import org.academiadecodigo.codezillas.Client.ClientRequest;
 
 public class RequestHandler {
+
+    private InputScanner lastInput;
 
     public InputScanner stringScanner(String message){
         StringInputScanner stringScanner = new StringInputScanner();
@@ -24,4 +27,31 @@ public class RequestHandler {
         return menuInput;
     }
 
+    public Object handleStart(ClientRequest clientRequest){
+
+        InputScanner scanner = null;
+
+        String command = clientRequest.getCommand();
+        String answerString = clientRequest.getAnswerString();
+        int answerInt = clientRequest.getAnswerInt();
+
+        if (lastInput == null) {
+            scanner = Navigation.loginRegisterMenu();
+            lastInput = scanner;
+            return scanner;
+        }
+
+        if (lastInput.equals(Navigation.loginRegisterMenu())) {
+            scanner = Navigation.clientMenu();
+            lastInput = scanner;
+            return scanner;
+        }
+        /*
+        switch (lastInput) {
+            case Navigation.loginRegisterMenu():
+                InputScanner i = Navigation.loginRegisterMenu();
+        }
+        */
+        return Navigation.loginRegisterMenu();
+    }
 }
